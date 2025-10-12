1d57e4e51a641d2730797cd5bc173f160f3f2041
Shankar-14964 <shankar14964@gmail.com>
2025-10-12 12:19:58 +0530

    Added Linear Search Module

---
+++ linearSearch.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[]={1,2,5,6,9,10};
+    int target = 6;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i+=1){
+        if(arr[i]==target){
+            cout<<"Element Found !!";
+        }
+    }
+    return 0;
+}
