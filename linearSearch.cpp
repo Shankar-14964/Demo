fdecf9a6152e13b887c4df38c4bf485e8d48a426
Shankar_14964 <shankar14964@gmail.com>
2025-10-12 16:22:19 +0530

    Added gitignore

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -6,10 +6,14 @@ int main(){
     cout<<"Enter target Val : \n";
     cin>>target;
     int n = sizeof(arr)/sizeof(arr[0]);
+    bool found = false;
     for(int i=0;i<n;i+=1){
         if(arr[i]==target){
             cout<<"Element Found !!";
+            found=true;
+            break;
         }
     }
+    if(!found) cout<<"Element not present";
     return 0;
 }
