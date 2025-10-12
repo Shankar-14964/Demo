06c2749b1f4982a8900c23cf0df8d50f7e915ca9
Shankar-14964 <shankar14964@gmail.com>
2025-10-12 12:24:33 +0530

    Modified Target

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 int main(){
     int arr[]={1,2,5,6,9,10};
-    int target = 6;
+    int target;
+    cout<<"Enter target Val : \n";
+    cin>>target;
     int n = sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<n;i+=1){
         if(arr[i]==target){
